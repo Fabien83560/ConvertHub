68a3ad183ce6ec91b126db8de6b591056045fed9
Fabien83560 <fabienortega.290604@gmail.com>
2024-09-02 17:10:08 +0200

    Change apache configuration

--- init-certbot.sh
+++ init-certbot.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 apachectl start
+
+sleep 10
+
 certbot --apache -d converthub.ortegaf.fr --non-interactive --agree-tos -m fabienortega.290604@gmail.com
-apache2-foreground
+
+apachectl restart
+
+while true; do
+  sleep 12h
+  certbot renew --quiet
+  apachectl restart
+done
